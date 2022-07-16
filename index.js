378497afe87863e2e3c16fd5e360ee651eb30f24
Aer-osa <eric.n.long@gmail.com>
2022-07-15 17:55:53 -0700

    Add morgan.token

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
-
+app.use(morgan('tiny'))
 app.use(express.json())
+morgan.token('object', function (req, res){
+  return `${JSON.stringify(req.body)}`
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 
 let persons = [
     { 
